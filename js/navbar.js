6aeda952f5049434972b9c438a0017ccb68a96da
Marre <marrebjoredal@gmail.com>
2025-03-12 11:08:03 +0100

    Enhance navbar functionality: implement close and open button actions, add overlay for improved user experience, and update footer with copyright information.(footer is not styled)

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,14 @@
+fetch("/includes/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
+  });
+
 function closeBtn() {
-    console.log("yo")
-    document.querySelector('nav > ul').style.display = 'none';
+  document.querySelector("nav > ul").style.display = "none";
+  document.querySelector(".overlay").style.display = "none";
 }
-
-fetch('/includes/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar').innerHTML = data;
-    });
-
+function openBtn() {
+  document.querySelector("nav > ul").style.display = "block";
+  document.querySelector(".overlay").style.display = "flex";
+}
