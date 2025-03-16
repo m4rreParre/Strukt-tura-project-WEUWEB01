a3b5be51f8920806c47bc8728f2f12d6b2333fb7
m4rreParre <marrebjoredal@gmail.com>
2025-03-16 20:24:43 +0100

    Enhance navbar accessibility and styling: add ARIA attributes for buttons, update overlay width, and improve footer layout with GitHub icon.

--- js/navbar.js
+++ js/navbar.js
@@ -7,8 +7,10 @@ fetch("/includes/navbar.html")
 function closeBtn() {
   document.querySelector("nav > ul").style.display = "none";
   document.querySelector(".overlay").style.display = "none";
+  document.querySelector("nav > ul").setAttribute('aria-expanded', 'false');
 }
 function openBtn() {
   document.querySelector("nav > ul").style.display = "block";
   document.querySelector(".overlay").style.display = "flex";
+  document.querySelector("nav > ul").setAttribute('aria-expanded', 'true');
 }
