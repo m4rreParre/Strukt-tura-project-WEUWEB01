7872a52162c0804f233eac152fe478b27625a451
m4rreParre <marrebjoredal@gmail.com>
2025-03-16 21:59:29 +0100

    Enhance responsive design: add dynamic <br> tag adjustment in typed.js and improve CSS for navigation and layout across various screen sizes.

--- js/typed.js
+++ js/typed.js
@@ -5,3 +5,20 @@ const typed1 = new Typed('#header-typed', {
     backDelay: 1500,
     loop: true
 });
+
+function adjustBRTag() {
+    const h2 = document.querySelector('h2');
+    const brTag = h2.querySelector('br');
+
+    if (window.innerWidth > 768 && brTag) {
+        // Ta bort <br>-taggen om skärmstorleken är över 768px
+        brTag.remove();
+    } else if (window.innerWidth <= 768 && !brTag) {
+        // Om skärmen är mindre än 768px, lägg till <br> om det inte finns
+        const newBr = document.createElement('br');
+        h2.appendChild(newBr);
+    }
+}
+
+window.addEventListener('resize', adjustBRTag);
+window.addEventListener('load', adjustBRTag);
